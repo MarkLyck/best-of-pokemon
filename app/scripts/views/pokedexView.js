eefaff0b9f0c670090b17bde8e9e8113739e6864
Mark Lyck <mark.lyck@gmail.com>
2016-07-23 18:54:47 -0500

    Created filter by dropdown (not functional yet)

--- app/scripts/views/pokedexView.js
+++ app/scripts/views/pokedexView.js
@@ -20,7 +20,33 @@ const PokedexView = Backbone.View.extend({
   },
   template: function() {
     return `
-    <h3 id="pokedex-title">All Pokemon</h3>
+    <div class="filter-options">
+      <dropdown>
+        <input id="toggle1" type="checkbox">
+        <label for="toggle1" class="animate">Filter by: <span id="filter-by-span">All</span><i class="fa fa-list float-right"></i></label>
+        <ul class="animate">
+          <li class="animate">All</li>
+          <li class="animate">Bug</li>
+          <li class="animate">Dark</li>
+          <li class="animate">Dragon</li>
+          <li class="animate">Electric</li>
+          <li class="animate">Fairy</li>
+          <li class="animate">Fighting</li>
+          <li class="animate">Fire</li>
+          <li class="animate">Flying</li>
+          <li class="animate">Ghost</li>
+          <li class="animate">Grass</li>
+          <li class="animate">Ground</li>
+          <li class="animate">Ice</li>
+          <li class="animate">Normal</li>
+          <li class="animate">Poison</li>
+          <li class="animate">Psychic</li>
+          <li class="animate">Rock</li>
+          <li class="animate">Steel</li>
+          <li class="animate">Water</li>
+        </ul>
+      </dropdown>
+    </div>
     <ul id="pokedex-list">
     </ul>
     <button id="load-more-btn">Load more Pokemon</button>
@@ -28,6 +54,25 @@ const PokedexView = Backbone.View.extend({
   },
   render: function() {
     this.$el.html(this.template())
+    // //TOGGLING NESTED ul
+    // this.$(".drop-down .selected a").click(function() {
+    //     this.$(".drop-down .options ul").toggle();
+    // });
+    //
+    // //SELECT OPTIONS AND HIDE OPTION AFTER SELECTION
+    // this.$(".drop-down .options ul li a").click(function() {
+    //     var text = $(this).html();
+    //     this.$(".drop-down .selected a span").html(text);
+    //     this.$(".drop-down .options ul").hide();
+    // });
+    //
+    //
+    // //HIDE OPTIONS IF CLICKED ANYWHERE ELSE ON PAGE
+    // $(document).bind('click', function(e) {
+    //     var $clicked = $(e.target);
+    //     if (! $clicked.parents().hasClass("drop-down"))
+    //         this.$(".drop-down .options ul").hide();
+    // });
     store.pokemons.data.each((pokemon) => {
       let $pokemonLi = $(`
           <li class="pokemon-li">
@@ -50,9 +95,11 @@ const PokedexView = Backbone.View.extend({
 
       $pokemonLi.find('.top').css('background-image', `url('assets/images/pokemon/${fixedNumber}.png')`);
       this.$('#pokedex-list').append($pokemonLi);
+
       $pokemonLi.find('.like-btn').on('click',function() {
         $pokemonLi.find('.like-btn').toggleClass('liked')
       });
+
       $pokemonLi.on('click', function (e) {
         if (!$(e.target).hasClass('like-btn')) {
           router.navigate(`pokemon/${$pokemonLi.find('.pokemon-number').text()}`, {trigger:true});
