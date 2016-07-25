d56a22d29bf02dfa9e146a098fb2791de8e75e76
Caryn Ligon <carynligon@gmail.com>
2016-07-24 22:46:03 -0500

    image issue fixed, now reordering issue

--- app/scripts/views/trainerView.js
+++ app/scripts/views/trainerView.js
@@ -1,5 +1,6 @@
 import Backbone from 'backbone';
 import $ from 'jquery';
+import _ from 'underscore';
 
 import router from '../router';
 import store from '../store';
@@ -20,20 +21,23 @@ const TrainerView = Backbone.View.extend({
     `;
   },
   render: function() {
+    _.sortBy(store.users.data, 'id');
     this.$el.html(this.template());
     store.users.data.each((user) => {
       if (!user.get('profileImg')) {
         user.set("profileImg", "https\://rebekahlang.files.wordpress.com/2015/08/pokemon-egg-png.png");
+        console.log(user);
       }
+      console.log(user);
       let $userLi = $(`
         <li class="user-thumbnail">
-          <div class="user-image">
+          <div class="user-image" id="${user.get('id')}">
           </div>
           <h3>${user.get('username')}</h3>
         </li>
         `);
       this.$('#trainer-list').append($userLi);
-      this.$('.user-image').css('background-image', `url("${user.get('profileImg')}")`);
+      this.$(`#${user.get('id')}`).css('background-image', `url("${user.get('profileImg')}")`);
       $userLi.on('click', function(e) {
         router.navigate(`trainer/${user.get('id')}`, {trigger:true});
       });
